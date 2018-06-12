5c23bef3a331d6a43264048704e96d6ecfa37225
ywang7 <ywang7@wal-mart.com>
2018-06-12 17:01:48 +0800

    add server and client

--- eureka-server/src/main/java/com/rick/framework/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/rick/framework/EurekaServerApplication.java
@@ -3,6 +3,8 @@ package com.rick.framework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
