a5914b6feaacda08640655b0bcfb61e071ca4f05
ywang7 <ywang7@wal-mart.com>
2018-06-15 15:48:08 +0800

    add server and client

--- consumer1/src/main/java/com/rick/framework/Consumer1Application.java
+++ consumer1/src/main/java/com/rick/framework/Consumer1Application.java
@@ -3,12 +3,16 @@ package com.rick.framework;
 //import com.rick.framework.config.ExcudeAnnotation;
 //import com.rick.framework.config.MyRibbonConfig;
 //import com.rick.framework.config.ExcudeAnnotation;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import com.rick.framework.config.ExcudeAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.HystrixMetricsProperties;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,7 +26,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrixDashboard
 // 使用此规则不可放在 可扫描的路径下, 如果非要放置, 需要加自定义注解
 //@RibbonClient(name = "SPRING-CLOUD-SERVER1", configuration = MyRibbonConfig.class)
-//@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {ExcudeAnnotation.class})})
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {ExcudeAnnotation.class})})
 //@ComponentScan(basePackages = {"com.rick.framework","org.springframework.cloud.client.loadbalancer"})
 public class Consumer1Application {
 
@@ -31,7 +35,14 @@ public class Consumer1Application {
     RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(Consumer1Application.class, args);
     }
+
+
+
+
 }
---
+++ consumer1/src/main/java/com/rick/framework/config/HystrixConfig.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: HystrixConfig
+ * Author:   ywang7
+ * Date:     2018/6/15 15:00
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.rick.framework.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author ywang7
+ * @create 2018/6/15
+ * @since 1.0.0
+ */
+@Configuration
+public class HystrixConfig {
+
+    @Bean
+    public HystrixMetricsStreamServlet hystrixMetricsStreamServlet(){
+        return new HystrixMetricsStreamServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean registration(HystrixMetricsStreamServlet servlet){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(servlet);
+        registrationBean.setEnabled(true);//是否启用该registrationBean
+        registrationBean.addUrlMappings("/hystrix.stream");
+        return registrationBean;
+    }
+}
